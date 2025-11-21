7117d4f6116f4b5c5419f412f00744b902b1c05c
Taufiqurahman-rh <rahm4n22@gmail.com>
2025-11-21 08:55:32 +0700

    script.js

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-const words = [
-    "Cybersecurity Enthusiast",
-    "Ethical Hacker Beginner",
-    "Networking",
-    "CTF Player"
-];
+// Efek glow saat hover pada nama
+const name = document.querySelector('.name');
 
-let i = 0;
-let j = 0;
-let current = "";
-let isDeleting = false;
+name.addEventListener('mouseover', () => {
+    name.style.textShadow = "0 0 25px #00eaff";
+});
 
-function type() {
-    const typing = document.getElementById("typing");
-
-    if (!isDeleting) {
-        current = words[i].slice(0, j++);
-    } else {
-        current = words[i].slice(0, j--);
-    }
-
-    typing.textContent = current;
-
-    let speed = isDeleting ? 60 : 90;
-
-    if (j === words[i].length + 1) {
-        isDeleting = true;
-        speed = 1200;
-    } else if (isDeleting && j === 0) {
-        isDeleting = false;
-        i = (i + 1) % words.length;
-    }
-
-    setTimeout(type, speed);
-}
-
-type();
+name.addEventListener('mouseout', () => {
+    name.style.textShadow = "none";
+});
