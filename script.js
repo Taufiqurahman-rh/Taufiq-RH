5c9d94c89f38990f533ae17bee2380cc0d95fdb9
Taufiqurahman-rh <rahm4n22@gmail.com>
2025-11-21 09:19:03 +0700

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function openSkill(id) {
     const allDetails = document.querySelectorAll('.skill-detail');
 
-    // Sembunyikan semua
     allDetails.forEach(detail => {
         detail.style.display = "none";
     });
-
-    // Tampilkan yang dipilih
+    
     const selected = document.getElementById(id);
     if (selected) {
         selected.style.display = "block";
         window.location.href = "#detail"; // auto scroll
     }
 }
+
