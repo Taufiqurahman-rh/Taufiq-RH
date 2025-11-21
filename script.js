1398dab5c0b1c00c236c324a1a833e26f3895d5e
Taufiqurahman-rh <rahm4n22@gmail.com>
2025-11-21 09:22:17 +0700

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function openSkill(id) {
-    const allDetails = document.querySelectorAll('.skill-detail');
-
-    allDetails.forEach(detail => {
-        detail.style.display = "none";
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
     });
-    
-    const selected = document.getElementById(id);
-    if (selected) {
-        selected.style.display = "block";
-        window.location.href = "#detail"; // auto scroll
-    }
-}
+});
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("muncul");
+        }
+    });
+});
+
+document.querySelectorAll(".skill-card").forEach(el => observer.observe(el));
+document.querySelectorAll(".hero, .contact").forEach(el => observer.observe(el));
